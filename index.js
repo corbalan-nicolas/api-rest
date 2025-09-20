149521d5b317f6dd6a82c254f5b2b0d418859ccd
corbalan-nicolas <nicolas.lcorbalan@davinci.edu.ar>
2025-09-19 23:28:08 -0300

    ¡Ups!

--- index.js
+++ index.js
@@ -39,36 +39,3 @@ app.listen(PORT, () => {
   ➜  Port:  ${PORT}
   `)
 })
-
-/* 
-# Parcial 1: TO DO
-Items para cumplir con la pauta de la consigna 1
-
-## Página principal
-  [ ] HTML que informe sobre la api y permita acceder a los endpoints mediante URL
-  [x] Pie de página con Nombre + apellido, materia, docente y comisión
-
-## Rutas
-  [ ] Mínimo 2 rutas
-  Funcionalidades requeridas en al menos una de las rutas (en mi caso mnemonics):
-    [x] Ver todos los documentos de una colección
-    [ ] Obtener un documento específico por su ID
-    [x] Actualizar la información de un documento
-    [x] Eliminar un documento
-    [x] Incluir al menos dos métodos de filtrado
-    [ ] Búsqueda por nombre
-
-## Autenticación
-  [ ] JSON Web Tokens
-
-## Validaciones
-  [ ] Incluír las validaciones necesarias
-
-
-
-
-  nicolaslcorbalan_db_user
-  GE2ZX58OV3QJ8nvk
-
-  mongodb+srv://nicolaslcorbalan_db_user:GE2ZX58OV3QJ8nvk@davinci.8hl9vww.mongodb.net/japanese_api?retryWrites=true&w=majority&appName=DaVinci
-*/
